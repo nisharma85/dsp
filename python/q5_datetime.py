a82d59ba0f867b9251aded25f310b992615eab38
nisharma85 <nitikasharma85@gmail.com>
2015-10-22 15:59:37 -0400

    Update q5_datetime.py

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -1,13 +1,29 @@
 # Hint:  use Google to find python function
+#####a
+date_start = '01-02-2013'    
+date_stop = '07-28-2015'
 
-####a) 
-date_start = '01-02-2013'  
-date_stop = '07-28-2015'   
+from datetime import datetime
+ y = datetime.strptime(date_start, '%m-%d-%Y')
+ z = datetime.strptime(date_stop, '%m-%d-%Y')
+ delta= z-y
+ print delta
+ 
+ #####b
+ date_start = '12312013'  
+date_stop = '05282015' 
 
-####b)  
-date_start = '12312013'  
-date_stop = '05282015'  
-
-####c)  
-date_start = '15-Jan-1994'  
+from datetime import datetime
+ y = datetime.strptime(date_start, '%m%d%Y')
+ z = datetime.strptime(date_stop, '%m%d%Y')
+ delta= z-y
+ print delta
+#####c
+date_start = '15-Jan-1994'      
 date_stop = '14-Jul-2015'  
+
+from datetime import datetime
+ y = datetime.strptime(date_start, '%d-%b-%Y')
+ z = datetime.strptime(date_stop, '%d-%b-%Y')
+ delta= z-y
+ print delta
