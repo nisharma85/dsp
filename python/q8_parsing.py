1ff52abb1a96dfaecd6789c50008a3677bb826b4
nisharma85 <nitikasharma85@gmail.com>
2015-11-04 21:06:00 -0500

    Update q8_parsing.py

--- python/q8_parsing.py
+++ python/q8_parsing.py
@@ -6,12 +6,45 @@
 
 
 import csv
+import os
+os.getcwd()
+os.chdir('C:/Users/shani16/Documents/Python_dsp')
 
-  def read_data(data):
+import csv
+
+
+def read_data(data):
+    with open(data, 'r') as f:
+        data = [row for row in csv.reader(f.read().splitlines())]
+    return data
+
+
+# ---- run code ---- #
+
+data = "football.csv"
+f  =read_data(data)        
    # COMPLETE THIS FUNCTION
 
-  def get_min_score_difference(self, parsed_data):
+def get_min_score_difference(self, parsed_data):
+    parsed_data.pop(0) #removes headers which are ['Team', 'Games', 'Wins'...
+    goals = [x[5] for x in parsed_data] #the x here is each "line" and you are telling it to make goals the 5 element in each line x
+    goals_allowed = [x[6]for x in parsed_data] #see above
+    values = [float(x) - float(y) for x, y in zip(goals, goals_allowed)] 
+    return values.index(min(values))
+      
+      
     # COMPLETE THIS FUNCTION
 
-  def get_team(self, index_value, parsed_data):
-    # COMPLETE THIS FUNCTION
+def get_team(self, index_value, parsed_data):
+    print parsed_data[0]
+    teams = [x[0] for x in parsed_data]
+    return teams[index_value]
+    
+import pandas as pd
+
+
+def smallest_gap(fname, col1, col2, result_col):
+    data = pd.read_csv(fname)
+    diff = data[col1] - data[col2]
+    smallest = data[diff == diff.min()]
+    return smallest[result_col].values[0]
