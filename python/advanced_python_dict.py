3aaface8a9384fff3a94dea2b93b4b41eb9e1fc2
nisharma85 <nitikasharma85@gmail.com>
2015-11-07 16:16:03 -0500

    Update advanced_python_dict.py

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -1,1 +1,25 @@
-PLACE YOUR CODE HERE
+################################################################
+#Part 3: Dictionaries
+
+data["las1"] = data["name"].str.split().str[-1]
+data["firs1"] = data["name"].str.split().str[0]
+faculty_dict= dict([(i,[a,b,c ]) for i, a,b,c in zip(data.las1,data.degree,data.title,data.email)])
+
+ print('The first three key and value pairs in first dictionary:')
+ print faculty_dict.items()[:3]
+ 
+##########################
+ data["name_new"] = data["firs1"].map(str) + ","+ data["las1"]
+professor_dict= dict([(i,[a,b,c ]) for i, a,b,c in zip(data.name_new,data.degree,data.title,data.email)])
+
+ print('The first three key and value pairs in second dictionary:')
+ print professor_dict.items()[:3]
+ 
+##########################
+ 
+professor_dict_sorted = dict(sorted(professor_dict.items(), key=operator.itemgetter(1)))
+
+ print('The first three key and value pairs in second dictionary sorted by last name:')
+ print professor_dict_sorted.items() [:3]
+ 
+################################################################ 
