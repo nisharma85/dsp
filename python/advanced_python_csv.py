1f8164f7f44f7d653cbaa78ac3c33db9f9d2f5bf
nisharma85 <nitikasharma85@gmail.com>
2015-11-07 16:15:36 -0500

    Update advanced_python_csv.py

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,7 @@
-PLACE YOUR CODE HERE
+################################################################
+#Part 2: Write to csv
+
+data['email'].to_csv("emails.csv", sep='\t', index=False) #Write email column to csv file
+
+
+################################################################
